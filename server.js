6d45db7672092d335ce27fc7a4ff5a7f96b0427a
Duncan Marsh <dwolfm@gmail.com>
2016-04-12 00:40:59 -0700

    init simple request logger

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+const http = require('http');
+const response = require(__dirname + '/lib/response.js');
+const logger = require(__dirname + '/lib/logger.js');
+
+const port = process.env.PORT || 7070; 
+
+const server = http.createServer((req, res) => {
+  logger.printReqHeaders(req);
+  
+  if (req.method === 'GET'){
+    response.reqBodyNo(res);
+    return;
+  }
+
+  req.on('end', () => {
+    console.log('wedone slone');
+  });
+
+  req.on('data', (data) => {
+    logger.printReqBody(data, (err) => {
+      if (err) return response.reqBodyBad(res);
+      response.reqBodyOk(res);
+    });
+  });
+});
+
+server.listen(port, function(){
+  console.log('request-loger is running on port:', port);
+});
